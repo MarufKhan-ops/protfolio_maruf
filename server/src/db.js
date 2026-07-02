80b69a4bc65a980ba2ddbc3202225fff3f76e633
Istiyak Hasan Maruf <69573296+MarufKhan-ops@users.noreply.github.com>
2026-07-02 18:42:15 +0600

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/maruf_portfolio";
+  const uri = process.env.MONGO_URI || "mongodb+srv://maruf333444:maruf333444@cluster0.eqz91gh.mongodb.net/maruf_portfolio";
   await mongoose.connect(uri);
   console.log("✅ MongoDB connected:", mongoose.connection.name);
 }
